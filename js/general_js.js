c0960d29a7697fc92d55160e2911bb26d07539e0
rabbitblood <70198795+rabbitblood@users.noreply.github.com>
2022-10-13 22:52:36 -0700

    Add files via upload

--- js/general_js.js
+++ js/general_js.js
@@ -1,10 +1,14 @@
 const nav_bar = document.getElementById("nav_bar");
 
-let nav_bar_expand_height = "200px";
+let nav_bar_expand_height = "300px";
 let nav_bar_expanded = false;
+let nav_bar_font_size = "10px";
+
+init();
 
 function init(){
     let nav_bar_expanded = false;
+    nav_bar.style.fontSize = "0";
 }
 
 function click_menu_button(){
@@ -12,6 +16,7 @@ function click_menu_button(){
     {
         nav_bar.style.height = "0px";
         nav_bar.style.opacity = "0";
+        nav_bar.style.fontSize = "0";
         nav_bar_expanded = false;
         
     }
@@ -19,6 +24,7 @@ function click_menu_button(){
     {
         nav_bar.style.height = nav_bar_expand_height;
         nav_bar.style.opacity = "1";
+        nav_bar.style.fontSize = nav_bar_font_size;
         nav_bar_expanded = true;
     }
 
