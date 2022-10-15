36ea58fd916b9edd03ba09fa42d58a42b5501f72
rabbitblood <70198795+rabbitblood@users.noreply.github.com>
2022-10-15 09:30:51 -0700

    Add files via upload

--- js/general_js.js
+++ js/general_js.js
@@ -1,4 +1,5 @@
 const nav_bar = document.getElementById("nav_bar");
+const background_image = document.getElementById("main_img_container");
 
 let nav_bar_expand_height = "300px";
 let nav_bar_expanded = false;
@@ -9,6 +10,20 @@ init();
 function init(){
     let nav_bar_expanded = false;
     nav_bar.style.fontSize = "0";
+
+    if(window.location.pathname.includes("contact")){
+        set_background_image("url(./../image/david_logo.png)");
+    }
+    else if(window.location.pathname.includes("photography")){
+        set_background_image("url(./../image/star_trait_small.jpg)");
+    }
+    else if(window.location.pathname.includes("about")){
+        set_background_image("url(./../image/star_trait_small.jpg)");
+    }
+}
+
+function set_background_image(image_url){
+    background_image.style.backgroundImage = image_url;
 }
 
 function click_menu_button(){
